43f94113b0eb1981a8a067a156f38caa28209426
unknown <shahidclt93@gmail.com>
2025-04-12 14:03:59 +0530

    app component path error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "./_components/Header";
-import Hero from "./_components/HEro";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
